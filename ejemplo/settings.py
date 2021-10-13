8917ae98dbd2b94b9fd28b9234351d81d0b7fb16
danielgaviri4 <gdaniel@unicauca.edu.co>
2021-10-13 15:17:28 -0500

    nueva modificacion

--- ejemplo/settings.py
+++ ejemplo/settings.py
@@ -139,6 +139,5 @@ REST_FRAMEWORK = {
     # ]
 }
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+importar django_heroku
+django_heroku.settings (locales ())
